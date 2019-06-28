1b71e66de03db841ead8e6f91dcaf2f363f6b7f2
oleksiirude <unitedcolorsofuatechno1988>
2019-06-28 22:46:16 +0300

    test

--- config/dbconfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	define('DSN', 'mysql:host=localhost;dbname=matcha;charset=utf8mb4');
-	define('USERNAME', 'root');
-	define('PASSWORD', 'root'); // home
-//	define('PASSWORD', 'qweqwe'); // unit
