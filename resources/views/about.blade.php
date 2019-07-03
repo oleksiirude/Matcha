788cff1067a02b3d0cf0fcc771d5c5de004068f8
oleksiirude <unitedcolorsofuatechno1988>
2019-07-03 16:44:51 +0300

    re-start

--- resources/views/about.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('base')
-
-@section('title', 'About page')
-
-@section('content')
-    <h1>About</h1>
-    <a href="/">Home</a>
-@endsection
--- resources/views/main.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('base')
-
-@section('title', 'Main page')
-
-@section('content')
-    <h1>Main page</h1>
-@endsection
