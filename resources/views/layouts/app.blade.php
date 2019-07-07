bf553c60c426b168953d9320ebb0f5af6b313daa
dashacherednichenko <dasha.cherednichenko@gmail.com>
2019-07-07 12:10:51 +0300

    fixed problems with logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img class="logo_img" src="images/heart.png">
+                    <img class="logo_img" src="{{ asset('images/heart.png') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
