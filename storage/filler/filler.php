a05d74a199abf47ad8a2201c17cd1d87d3a47160
dashacherednichenko <dasha.cherednichenko@gmail.com>
2019-07-29 10:30:29 +0300

    tmp

--- storage/filler/filler.php
+++ storage/filler/filler.php
@@ -2,9 +2,9 @@
 	
 	require_once('data.php');
 
-	define('DSN', 'mysql:host=127.0.0.1;dbname=db_matcha;port=3306;charset=utf8mb4');
+	define('DSN', 'mysql:host=localhost;dbname=db_matcha;unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;charset=utf8mb4');
 	define('USERNAME', 'root');
-	define('PASSWORD', 'Trofimov2015');
+	define('PASSWORD', 'root');
 
 	$connection = new PDO(DSN, USERNAME, PASSWORD, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 	
